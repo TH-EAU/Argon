59df33e9a21d450d0accb49a697c716500c85193
Théau NICOLAS <theaunicolas11@gmail.com>
2020-12-10 20:16:40 +0100

    Argon

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  admin : {
+    mail : {
+      address : "your.address@mail.com",
+      smtp : "",
+      host : ""
+    }
+  }
+};
---
+++ src/lib/authControl.js
@@ -0,0 +1,5 @@
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
+// const config = require('../config/config');
+
+exports.authenticate = () => true;
